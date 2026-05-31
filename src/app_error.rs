73619010f2dc2a62ff34473e84815e5f13eb6733
itsbugaring-cloud <itsbugaring@gmail.com>
2026-05-31 23:22:37 +0700

    Handle sqlx migration errors in app error type

--- src/app_error.rs
+++ src/app_error.rs
@@ -10,6 +10,8 @@ use thiserror::Error;
 pub enum AppError {
     #[error("database error: {0}")]
     Database(#[from] sqlx::Error),
+    #[error("database migration error: {0}")]
+    DatabaseMigration(#[from] sqlx::migrate::MigrateError),
     #[error("http client error: {0}")]
     HttpClient(#[from] reqwest::Error),
     #[error("io error: {0}")]
@@ -39,6 +41,7 @@ impl IntoResponse for AppError {
             AppError::Upstream(_) => StatusCode::BAD_GATEWAY,
             AppError::Multipart(_) => StatusCode::BAD_REQUEST,
             AppError::Database(_)
+            | AppError::DatabaseMigration(_)
             | AppError::HttpClient(_)
             | AppError::Io(_)
             | AppError::Internal(_) => StatusCode::INTERNAL_SERVER_ERROR,
